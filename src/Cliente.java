a5c51267ba2b9547d0a170dcdf4970deb200b9c5
Ana Is Writing Code <168542009+anaiswritingcode@users.noreply.github.com>
2025-11-09 17:00:06 +0100

    Añadir getters y setters para Cliente y poner más info en toString() de Cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Cliente {
   private int idCliente;
   private String nombreCompleto;
-  private List<String> direccion = new ArrayList<>();
+  private List<String> direcciones = new ArrayList<>();
   private String correo;
   // En proceso.
 
@@ -13,6 +13,26 @@ public class Cliente {
     this.idCliente = idCliente;
     this.nombreCompleto = nombreCompleto;
     this.correo = correo;
+    // En proceso.
+  }
+
+  // * Getters:
+
+  public List<String> getDirecciones() {
+    return direcciones;
+  }
+
+  // * Setters:
+
+  public void setDireccion(String direccion) {
+    direcciones.add(direccion);
+  }
+
+  // * Otros métodos:
+
+  @Override // Sobreescribe el método toString() propio de objetos en Java.
+  public String toString() {
+    return "\nCliente: " + nombreCompleto + ", correo: " + correo + ", ID: " + idCliente + ".";
     // En proceso.
   }
 }
