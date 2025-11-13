a48f685ea48fa80c9c8f7d85264e0efae5677cbb
Ana <168542009+anaiswritingcode@users.noreply.github.com>
2025-11-13 11:25:56 +0100

    Disminuir sangría de 4 a 2 espacios en Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // En proceso.
-    }
+  public static void main(String[] args) throws Exception {
+    // En proceso.
+  }
 }
