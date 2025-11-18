9b26c41b85f221a3166a1ab7fd46827d75fbf57a
Ana <168542009+anaiswritingcode@users.noreply.github.com>
2025-11-18 13:10:10 +0100

    Implementar funcionalidad de añadir producto por teclado

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,14 @@ public class Main {
     int respuestaMenu = 0;
     String nombreCliente = "";
     String correoCliente = "";
+    String tipoProducto = "";
+    String nombreProducto = "";
+    double precioProducto = 0;
+    double tamannoDescargaProducto = 0;
+    String licenciaProducto = "";
+    double costeEnvioProducto = 0;
     List<Cliente> listaClientes = new ArrayList<>();
+    List<Producto> listaProductos = new ArrayList<>();
 
     Scanner scan = new Scanner(System.in);
 
@@ -20,14 +27,14 @@ public class Main {
     do {
       System.out.print("\nSu respuesta (1, 2): ");
       respuestaMenu = scan.nextInt();
-      scan.nextLine();
+      scan.nextLine(); // Limpiar buffer.
 
       if (respuestaMenu != 1 && respuestaMenu != 2) {
-        System.out.println("Opción no válida, introduzca '1' o '2'.");
+        System.out.println("\nOpción no válida, introduzca '1' o '2'.");
       }
     } while (respuestaMenu != 1 && respuestaMenu != 2);
 
-    if (respuestaMenu == 1) {
+    if (respuestaMenu == 1) { // Añadir cliente.
       System.out.print("\n¿Cuál es el nombre completo del cliente? ");
       nombreCliente = scan.nextLine();
 
@@ -35,6 +42,40 @@ public class Main {
       correoCliente = scan.nextLine();
 
       listaClientes.add(new Cliente(nombreCliente, correoCliente));
+    } else { // Añadir producto.
+      do { // Elegir tipo de producto.
+        System.out.print("\n¿Qué tipo de producto desea añadir? (digital, fisico) ");
+        tipoProducto = scan.nextLine();
+
+        if (!tipoProducto.equalsIgnoreCase("digital") && !tipoProducto.equalsIgnoreCase("fisico")) {
+          System.out.println("\nOpción no valida. Escriba 'digital' o 'fisico' (sin tilde).");
+        }
+      } while (!tipoProducto.equalsIgnoreCase("digital") && !tipoProducto.equalsIgnoreCase("fisico"));
+
+      System.out.print("- Nombre del producto: ");
+      nombreProducto = scan.nextLine();
+
+      System.out.print("- Precio del producto: ");
+      precioProducto = scan.nextDouble(); // TODO: Verificar que no sea negativo.
+      scan.nextLine(); // Limpiar buffer.
+
+      if (tipoProducto.equalsIgnoreCase("digital")) { // Pedir atributos específicos de los productos digitales.
+        System.out.print("- Tamaño de descarga (en MB): ");
+        tamannoDescargaProducto = scan.nextDouble(); // TODO: Verificar que no sea negativo.
+        scan.nextLine(); // Limpiar buffer.
+
+        System.out.print("- Licencia: ");
+        licenciaProducto = scan.nextLine();
+
+        listaProductos.add(new ProductoDigital(nombreProducto, precioProducto, tamannoDescargaProducto, licenciaProducto));
+      }
+      
+      if (tipoProducto.equalsIgnoreCase("fisico")) { // Pedir atributos específicos de los productos físicos.
+        System.out.print("- Coste de envío: ");
+        costeEnvioProducto = scan.nextDouble(); // TODO: Verificar que no sea negativo.
+
+        listaProductos.add(new ProductoFisico(nombreProducto, precioProducto, costeEnvioProducto));
+      }
     }
 
     scan.close();
