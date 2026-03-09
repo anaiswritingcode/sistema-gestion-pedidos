7369b8da13ed3b98dcc12e07068c069eb23cd2bf
Ana Is Writing Code <168542009+anaiswritingcode@users.noreply.github.com>
2026-03-09 12:24:43 +0100

    Refactorización de Producto

--- src/main/java/com/sgp/producto/Producto.java
+++ src/main/java/com/sgp/producto/Producto.java
@@ -1,16 +1,28 @@
 package com.sgp.producto;
 
 public abstract class Producto {
-  private int idProducto;
-  private static int contadorId = 0;
+  private static int contadorId = 0; // 'static' para que se comparta entre instancias.
+  private final int idProducto;
   private String nombre;
   private double precio;
 
   // * Constructor:
 
   public Producto(String nombre, double precio) {
-    contadorId++;
-    idProducto = contadorId;
+
+    // Validación interna de 'nombre':
+
+    if (nombre == null || nombre.isEmpty()) {
+      throw new IllegalArgumentException("El nombre no puede estar vacío.");
+    }
+
+    // Validación interna de 'precio':
+
+    if (precio <= 0) {
+      throw new IllegalArgumentException("El precio debe ser positivo y mayor que 0.");
+    }
+
+    this.idProducto = contadorId++;
     this.nombre = nombre;
     this.precio = precio;
   }
@@ -29,15 +41,28 @@ public abstract class Producto {
     return precio;
   }
 
+  // * Setters:
+
+  public void setNombre(String nuevoNombre) { // Para asignarle un nuevo nombre al producto.
+    if (nuevoNombre == null || nuevoNombre.isEmpty()) {
+      throw new IllegalArgumentException("El nombre no puede estar vacío.");
+    }
+    this.nombre = nuevoNombre;
+  }
+
+  public void setPrecio(double nuevoPrecio) { // Para asignarle un nuevo precio al producto.
+    if (nuevoPrecio <= 0) {
+      throw new IllegalArgumentException("El precio debe ser positivo y mayor que 0.");
+    }
+    this.precio = nuevoPrecio;
+  }
+
   // * Otros métodos:
 
   @Override // Sobreescribe el método toString() propio de objetos en Java.
   public String toString() {
-    return "\nProducto: " + nombre + ", precio: " + precio + ".";
+    return String.format("Producto: %s, precio %.2f", nombre, precio);
   }
 
-  public double calcularPrecioFinal() {
-    return precio; // Aquí simplemente devuelve 'precio', se sobreescribe en las subclases
-                   // 'ProductoFisico' y 'ProductoDigital' según sus atributos específicos.
-  }
+  protected abstract double calcularPrecioFinal();
 }
