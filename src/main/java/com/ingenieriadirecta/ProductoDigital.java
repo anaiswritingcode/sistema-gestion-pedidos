ada398b81e2e39e502629e6a58026bfb810b0487
Ana <168542009+anaiswritingcode@users.noreply.github.com>
2026-03-11 09:49:34 +0100

    Añadir 'return' vacíos en archivos de ingenieriadirecta por errores de compilación

--- src/main/java/com/ingenieriadirecta/ProductoDigital.java
+++ src/main/java/com/ingenieriadirecta/ProductoDigital.java
@@ -7,10 +7,10 @@ public class ProductoDigital {
   private double descuento;
 
   public void ProductoDigital(String nombre, double precio, double tamannoDescarga, String licencia) {
-    // ...
+    return;
   }
 
   public double calcularPrecioFinal() {
-    // ...
+    return 0.0;
   }
 }
