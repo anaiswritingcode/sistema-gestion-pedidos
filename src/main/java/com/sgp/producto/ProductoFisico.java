b54ddaa0fb00ff18034d57818d8ffd7c0e88e85b
Ana <168542009+anaiswritingcode@users.noreply.github.com>
2026-05-15 10:28:01 +0200

    Refactorización de ProductoFisico y sus implementaciones

--- src/main/java/com/sgp/producto/ProductoFisico.java
+++ src/main/java/com/sgp/producto/ProductoFisico.java
@@ -2,42 +2,108 @@ package com.sgp.producto;
 
 public class ProductoFisico extends Producto {
   private double costeEnvio;
+  private double peso;
+  private String zonaDestino;
 
-  // * Constructor:
+  // * CONSTRUCTOR:
 
-  public ProductoFisico(String nombre, double precio, double costeEnvio) {
-    super(nombre, precio);
+  public ProductoFisico(String nombre, double precioBase, double peso, String zonaDestino) {
 
-    // Validación interna de 'costeEnvio':
+    super(nombre, precioBase);
 
-    if (costeEnvio <= 0) {
-      throw new IllegalArgumentException("El coste de envío debe ser positivo y mayor que 0.");
+    // Validación interna de 'peso':
+    if (peso <= 0) {
+      throw new IllegalArgumentException("El peso debe ser positivo.");
+    } else {
+      this.costeEnvio += peso;
+    }
+
+    // Validación interna de 'zonaDestino':
+    if (zonaDestino == null || zonaDestino.isEmpty()) {
+      throw new IllegalArgumentException("La zona de destino no puede estar vacía.");
+    } else if (zonaDestino.equalsIgnoreCase("ESPAÑA") || zonaDestino.equalsIgnoreCase("ESPA?A")) {
+      zonaDestino = "ESPANA";
+    }
+
+    switch (zonaDestino.toUpperCase()) {
+      case "ESPANA" -> this.costeEnvio += 0.0;
+      case "FRANCIA" -> this.costeEnvio += 5.0;
+      case "ITALIA" -> this.costeEnvio += 5.0;
+      case "PORTUGAL" -> this.costeEnvio += 5.0;
+      default -> this.costeEnvio += 10.0;
     }
 
     // Asignación de variables:
-
-    this.costeEnvio = costeEnvio;
+    this.peso = peso;
   }
 
-  // * Getters:
+  // * GETTERS:
 
+  /*
+   ** Recalcula el coste de envío con cada llamada, por si se han cambiado
+   ** los valores de peso y/o zona de envío:
+   */
   public double getCosteEnvio() {
-    return costeEnvio;
-  }
+    double costeEnvioActual;
 
-  // * Setters:
-
-  public void setCosteEnvio(double nuevoCosteEnvio) {
-    if (nuevoCosteEnvio <= 0) {
-      throw new IllegalArgumentException("El coste de envío debe ser positivo y mayor que 0.");
+    switch (this.zonaDestino) {
+      case "ESPANA" -> costeEnvioActual = 0.0;
+      case "FRANCIA" -> costeEnvioActual = 5.0;
+      case "ITALIA" -> costeEnvioActual = 5.0;
+      case "PORTUGAL" -> costeEnvioActual = 5.0;
+      default -> costeEnvioActual = 10.0;
     }
-    this.costeEnvio = nuevoCosteEnvio;
+
+    return this.peso + costeEnvioActual;
   }
 
-  // * Otros métodos:
+  public double getPeso() {
+    return peso;
+  }
 
-  @Override // Sobreescribe de Producto para hacer cálculos con los atributos específicos.
+  public String getZonaDestino() {
+    if (this.zonaDestino.equalsIgnoreCase("ESPANA")) {
+      return "ESPAÑA";
+    } else {
+      return zonaDestino.toUpperCase();
+    }
+  }
+
+  // * SETTERS:
+
+  /**
+   * Asigna un nuevo peso al producto.
+   * 
+   * @param nuevoPeso
+   */
+  public void setPeso(double nuevoPeso) {
+    if (nuevoPeso <= 0) {
+      throw new IllegalArgumentException("El peso debe ser positivo.");
+    }
+    this.peso = nuevoPeso;
+  }
+
+  /**
+   * Asigna una nueva zona de destino al producto.
+   * 
+   * @param nuevaZonaDestino
+   */
+  public void setZonaDestino(String nuevaZonaDestino) {
+    if (nuevaZonaDestino == null || nuevaZonaDestino.isEmpty()) {
+      throw new IllegalArgumentException("La zona de destino no puede estar vacía.");
+    } else if (nuevaZonaDestino.equalsIgnoreCase("ESPAÑA") || nuevaZonaDestino.equalsIgnoreCase("ESPA?A")) {
+      this.zonaDestino = "ESPANA";
+    } else {
+      this.zonaDestino = nuevaZonaDestino.toUpperCase();
+    }
+  }
+
+  // * OTROS MÉTODOS:
+
+  // Sobreescribe el de Producto para calcular con los atributos específicos:
+  @Override
   public double calcularPrecioFinal() {
-    return Math.round((getPrecio() + costeEnvio) * 100.0) / 100.0;
+    double precioTotal = getPrecio() + getCosteEnvio();
+    return Math.round(precioTotal * 100.0) / 100.0;
   }
 }
