1d3fe72b78d29eaa6ead5f99927ae4a83b039aca
Ana <168542009+anaiswritingcode@users.noreply.github.com>
2026-05-15 09:28:37 +0200

    Refactorización de Producto y sus implementaciones

--- src/main/java/com/sgp/producto/Producto.java
+++ src/main/java/com/sgp/producto/Producto.java
@@ -2,37 +2,34 @@ package com.sgp.producto;
 
 public abstract class Producto {
   private static int contadorId = 0; // 'static' para que se comparta entre instancias.
-  private final int idProducto;
+  private final int id;
   private String nombre;
-  private double precio;
+  private double precioBase;
 
-  // * Constructor:
+  // * CONSTRUCTOR:
 
-  protected Producto(String nombre, double precio) {
+  protected Producto(String nombre, double precioBase) {
 
     // Validación interna de 'nombre':
-
     if (nombre == null || nombre.isEmpty()) {
       throw new IllegalArgumentException("El nombre no puede estar vacío.");
     }
 
     // Validación interna de 'precio':
-
-    if (precio <= 0) {
-      throw new IllegalArgumentException("El precio debe ser positivo y mayor que 0.");
+    if (precioBase <= 0) {
+      throw new IllegalArgumentException("El precio debe ser positivo.");
     }
 
     // Asignación de variables:
-
-    this.idProducto = contadorId++;
+    this.id = contadorId++;
     this.nombre = nombre;
-    this.precio = precio;
+    this.precioBase = precioBase;
   }
 
-  // * Getters:
+  // * GETTERS:
 
   public int getIdProducto() {
-    return idProducto;
+    return id;
   }
 
   public String getNombre() {
@@ -40,31 +37,49 @@ public abstract class Producto {
   }
 
   public double getPrecio() {
-    return precio;
+    return precioBase;
   }
 
-  // * Setters:
+  // * SETTERS:
 
-  public void setNombre(String nuevoNombre) { // Para asignarle un nuevo nombre al producto.
+  /**
+   * Asigna un nuevo nombre al producto.
+   * 
+   * @param nuevoNombre
+   */
+  public void setNombre(String nuevoNombre) {
     if (nuevoNombre == null || nuevoNombre.isEmpty()) {
       throw new IllegalArgumentException("El nombre no puede estar vacío.");
     }
     this.nombre = nuevoNombre;
   }
 
-  public void setPrecio(double nuevoPrecio) { // Para asignarle un nuevo precio al producto.
+  /**
+   * Asigna un nuevo precio al producto.
+   * 
+   * @param nuevoPrecio
+   */
+  public void setPrecioBase(double nuevoPrecio) {
     if (nuevoPrecio <= 0) {
-      throw new IllegalArgumentException("El precio debe ser positivo y mayor que 0.");
+      throw new IllegalArgumentException("El precio debe ser positivo.");
     }
-    this.precio = nuevoPrecio;
+    this.precioBase = nuevoPrecio;
   }
 
-  // * Otros métodos:
+  // * OTROS MÉTODOS:
 
-  @Override // Sobreescribe el método toString() propio de objetos en Java.
+  // Sobreescribe el método toString() propio de objetos en Java:
+  @Override
   public String toString() {
-    return String.format("Producto: %s, precio %.2f", nombre, precio);
+    return String.format("Producto: %s, precio %.2f", nombre, precioBase);
   }
 
+  /**
+   * Método para ser sobreescrito por las distintas subclases de producto
+   * con los datos adicionales específicos de cada una a la hora de
+   * calcular el precio final.
+   * 
+   * @return
+   */
   public abstract double calcularPrecioFinal();
 }
--- src/test/java/com/sgp/producto/ProductoTest.java
+++ src/test/java/com/sgp/producto/ProductoTest.java
@@ -5,48 +5,48 @@ import org.junit.jupiter.api.Test;
 
 class ProductoTest {
 
-  @Test
-  void testCreacionProductoBase() { // * CP-10 se incluye dentro pero sin AssertFalse.
+    @Test
+    void testCreacionProductoBase() { // * CP-10 se incluye dentro pero sin AssertFalse.
 
-    String nombre1 = "";
-    String nombre2 = null;
-    double precio1 = 0;
-    double precio2 = -1;
+        String nombre1 = "";
+        String nombre2 = null;
+        double precio1 = 0;
+        double precio2 = -1;
 
-    /*
-     * Verificamos que saltan excepciones al crear
-     * productos con datos base inválidos:
-     */
+        /*
+         * Verificamos que saltan excepciones al crear
+         * productos con datos base inválidos:
+         */
 
-    assertThrows(IllegalArgumentException.class,
-        () -> new ProductoDigital(nombre1, precio1, 1.0, "Todos los derechos reservados"),
-        "Tendría que haber saltado una IllegalArgumentException al crear un producto con nombre y precio inválidos.");
+        assertThrows(IllegalArgumentException.class,
+                () -> new ProductoDigital(nombre1, precio1, 1.0, "Todos los derechos reservados"),
+                "Tendría que haber saltado una IllegalArgumentException al crear un producto con nombre y precio inválidos.");
 
-    assertThrows(IllegalArgumentException.class,
-        () -> new ProductoFisico(nombre2, precio2, 2.0),
-        "Tendría que haber saltado una IllegalArgumentException al crear un producto con nombre y precio inválidos.");
-  }
+        assertThrows(IllegalArgumentException.class,
+                () -> new ProductoFisico(nombre2, precio2, 2.0),
+                "Tendría que haber saltado una IllegalArgumentException al crear un producto con nombre y precio inválidos.");
+    }
 
-  @Test
-  void testSettersProductoBase() {
+    @Test
+    void testSettersProductoBase() {
 
-    String nombre = "Monitor 4k";
-    double precio = 350.0;
+        String nombre = "Monitor 4k";
+        double precio = 350.0;
 
-    // Verificamos que funcionen los setters de Producto:
+        // Verificamos que funcionen los setters de Producto:
 
-    ProductoFisico producto = new ProductoFisico(nombre, precio, 12.0);
+        ProductoFisico producto = new ProductoFisico(nombre, precio, 12.0);
 
-    assertThrows(IllegalArgumentException.class,
-        () -> producto.setNombre(""),
-        "Tendría que haber saltado una IllegalArgumentException al establecer un nuevo nombre vacío.");
+        assertThrows(IllegalArgumentException.class,
+                () -> producto.setNombre(""),
+                "Tendría que haber saltado una IllegalArgumentException al establecer un nuevo nombre vacío.");
 
-    assertThrows(IllegalArgumentException.class,
-        () -> producto.setNombre(null),
-        "Tendría que haber saltado una IllegalArgumentException al establecer un nuevo nombre de valor nulo.");
+        assertThrows(IllegalArgumentException.class,
+                () -> producto.setNombre(null),
+                "Tendría que haber saltado una IllegalArgumentException al establecer un nuevo nombre de valor nulo.");
 
-    assertThrows(IllegalArgumentException.class,
-        () -> producto.setPrecio(-1),
-        "Tendría que haber saltado una IllegalArgumentException al establecer un nuevo precio inválido.");
-  }
+        assertThrows(IllegalArgumentException.class,
+                () -> producto.setPrecioBase(-1),
+                "Tendría que haber saltado una IllegalArgumentException al establecer un nuevo precio inválido.");
+    }
 }
